ce1172e6593285ab3cdb9822cb05e9800055b2bd
shun414 <shunshira14@gmail.com>
2021-11-14 17:18:21 +0900

    add sessions create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'now'
+    end
+  end
 end
