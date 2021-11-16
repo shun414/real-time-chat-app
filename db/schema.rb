7be5c396f075aaa80ac444976d897e8422b0f7f8
shun414 <shunshira14@gmail.com>
2021-11-16 22:06:09 +0900

    add messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_14_051214) do
+ActiveRecord::Schema.define(version: 2021_11_16_125519) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_11_14_051214) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "users"
 end
