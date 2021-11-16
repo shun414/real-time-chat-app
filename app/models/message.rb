06418c9a41d9d680c0b9629efc879a637e580fdb
shun414 <shunshira14@gmail.com>
2021-11-16 22:39:43 +0900

    display messeges

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
-  validates :body, presence: true, length: { minimum: 1, maximum: 100 },
+  validates :body, presence: true, length: { minimum: 1, maximum: 100 }
 end
