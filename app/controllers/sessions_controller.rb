8f3088e44906ee6403f3d31060a2f0bf943714aa
shun414 <shunshira14@gmail.com>
2021-11-23 20:52:24 +0900

    add sessions controller and restrict user for chat room

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully"
+      flash[:success] = "Logged in successfully"
       redirect_to root_path
     else
-      flash.now[:alert] = "There was something wrong with your login details"
-      render 'now'
+      flash.now[:error] = 'There was something wrong with your login details'
+      render 'new'
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have successfully Logged out"
+    redirect_to login_path
+  end
+
 end
