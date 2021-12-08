4dfa38c5b593e779eb2c37d103f60c26cc61cdd5
shun414 <shunshira14@gmail.com>
2021-12-08 22:04:33 +0900

    remove debug code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,6 @@ before_action :require_user
 
 def create
   message = current_user.messages.build(message_params)
-  pp message
   if message.save
     ActionCable.server.broadcast "chatroom_channel",
                                   mod_message: message_render(message)
